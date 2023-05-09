ec12701922d04fca6657b95c669b252483b874dd
Richard An <richard.an96@gmail.com>
2023-05-08 22:44:31 -0400

    Add new storage for words

---
+++ main.js
@@ -0,0 +1,8 @@
+const startingWords = ["You will have a ",
+"There is ",
+"Something ", 
+]
+
+const adjectives = ["amazing", "awesome", "great", "wonderful", "terrific", "terrible", "bad", "horrible"]
+let randomNum = Math.floor(Math.random() * startingWords.length)
+console.log(randomNum)
