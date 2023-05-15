626594b92b5b71f5a68dc10e85770f147a2d535d
Richard An <richard.an96@gmail.com>
2023-05-15 14:47:08 -0400

    Add Helper Random Function, Add Message Generator

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-const startingWords = ["You will have a ",
-"There is ",
-"Something ", 
+const adjectives = ["amazing", "awesome", "great", "wonderful", "terrific", "terrible", "bad", "horrible"]
+const  adverbs = ["today", "tomorrow", "in the future", "soon" ]
+const nouns = ["day", "week", "surprise", "fortune", "friend" ]
+const startingWords = ["You will have a(n) ",
+"There is a(n) ",
+"Something " 
 ]
 
-const adjectives = ["amazing", "awesome", "great", "wonderful", "terrific", "terrible", "bad", "horrible"]
-let randomNum = Math.floor(Math.random() * startingWords.length)
-console.log(randomNum)
+//Helper Function to generate random choices from lists
+function pickRandom(list)
+{
+    return list[Math.floor(Math.random() * list.length)]
+}
+
+//Function takes in 4 lists and randomly picks values from each to form a coherent fortune from a fortune cookie
+function messageMaker(adjectiveList, nounList, adverbList, startWordList){
+    randAdj = pickRandom(adjectiveList)
+    randNoun = pickRandom(nounList)
+    randAdv = pickRandom(adverbList)
+    randSent = pickRandom(startWordList)
+    return randSent + randAdj + " " + randNoun + " " + randAdv
+}
+
+
+
+console.log(messageMaker(adjectives, nouns, adverbs, startingWords))
